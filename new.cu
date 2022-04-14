00f3916789088946eeec3423646396e55f106adc
gbnm2001 <78373993+gbnm2001@users.noreply.github.com>
2022-04-14 23:39:23 +0530

    Add files via upload

--- new.cu
+++ new.cu
@@ -16,29 +16,23 @@ bool comp(const pair<float, float> &a, const pair<float, float> &b)
     return a.second < b.second;
 }
 
-__global__ void filter(int *data_image, int *range, float query_grey, int row, int col, int query_row, int query_col, float TH2, char *filtered, int start_row)
+__global__ void filter(int *data_image, int *range, float query_grey, int subrow, int col, int query_row, int query_col, float TH2, char *filtered, int start_row, int real_row)
 { // filer the candidates for calculting the RMSD
     int y = blockIdx.y * blockDim.y + threadIdx.y;
-    int x = blockIdx.x * blockDim.x + threadIdx.x + start_row;
-    if (x >= row - query_row || y >= col - query_col)
-    {
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    if (x >= subrow || y >= col - query_col || x+start_row>=real_row - query_row){
         return;
     }
     int theta = blockIdx.z;
     float grey_sum = 0;
     for (int Y = y + range[theta]; Y < y + range[3 + theta]; Y++)
     { // grey avg of image
-        for (int X = x + range[6 + theta]; X < x + range[9 + theta]; X++)
-        {
-            for (int color = 0; color < 3; color++)
-            {
-
-                if (X >= 0 && Y >= 0 && X < row && Y < col)
-                {
-                    grey_sum += data_image[(row - 1 - X) * (col)*3 + Y * 3 + color];
+        for (int X = x + range[6 + theta]; X < x + range[9 + theta]; X++){
+            for (int color = 0; color < 3; color++){
+                if (X >= 0 && Y >= 0 && X < subrow && Y < col){
+                    grey_sum += data_image[(subrow - 1 - X) * (col)*3 + Y * 3 + color];
                 }
-                else
-                {
+                else{
                     grey_sum += 255;
                 }
             }
@@ -46,13 +40,11 @@ __global__ void filter(int *data_image, int *range, float query_grey, int row, i
     }
     grey_sum = grey_sum / ((range[9 + theta] - range[6 + theta]) * (range[3 + theta] - range[theta]) * 3);
 
-    if ((grey_sum - query_grey) <= TH2 && (grey_sum - query_grey) >= -1 * TH2)
-    {
-        filtered[(row - 1 - x) * col * 3 + y * 3 + theta] = 1;
+    if ((grey_sum - query_grey) <= TH2 && (grey_sum - query_grey) >= -1 * TH2){
+        filtered[(start_row + subrow - 1 - x) * col * 3 + y * 3 + theta] = 1;
     }
-    else
-    {
-        filtered[(row - 1 - x) * col * 3 + y * 3 + theta] = 0;
+    else{
+        filtered[(start_row +subrow - 1 - x) * col * 3 + y * 3 + theta] = 0;
     }
 }
 
@@ -72,10 +64,8 @@ __global__ void RMSDselect(int *data_image, int *query_image, float *cossin,
     top[index] = -1;
     float square_diff = 0.0;
 
-    for (int qx = 0; qx < query_row; qx++)
-    {
-        for (int qy = 0; qy < query_col; qy++)
-        {
+    for (int qx = 0; qx < query_row; qx++){
+        for (int qy = 0; qy < query_col; qy++){
             float rotated_x = x + qx * cossin[theta] + qy * cossin[3 + theta]; // rotated x
             float rotated_y = y + qy * cossin[theta] - qx * cossin[3 + theta];
             int x0 = floor(rotated_x);
@@ -85,31 +75,24 @@ __global__ void RMSDselect(int *data_image, int *query_image, float *cossin,
             float color_val = 0.0;
             float query_val = 0.0;
 
-            for (int c = 0; c < 3; c++)
-            {
+            for (int c = 0; c < 3; c++){
                 color_val = 255;
                 int z00 = 255, z01 = 255, z10 = 255, z11 = 255;
-                if (x0 == x1 && y0 == y1)
-                {
-                    if (y0 >= 0 && x0 >= 0 && y0 < col && x0 < row)
-                    {
+                if (x0 == x1 && y0 == y1){
+                    if (y0 >= 0 && x0 >= 0 && y0 < col && x0 < row){
                         color_val = data_image[(row - 1 - x0) * col * 3 + y0 * 3 + c];
                     }
                 }
-                else if (y0 == y1 && y0 >= 0 && y0 < col)
-                {
-                    if (x0 >= 0 && x0 < row)
-                    {
+                else if (y0 == y1 && y0 >= 0 && y0 < col){
+                    if (x0 >= 0 && x0 < row){
                         z00 = data_image[(row - x0 - 1) * col * 3 + y0 * 3 + c];
                     }
-                    if (x1 >= 0 && x1 < row)
-                    {
+                    if (x1 >= 0 && x1 < row){
                         z10 = data_image[(row - x1 - 1) * col * 3 + y0 * 3 + c];
                     }
                     color_val = (x1 - rotated_x) * z00 + (rotated_x - x0) * z10;
                 }
-                else if (x0 == x1 && x0 >= 0 && x0 < row)
-                {
+                else if (x0 == x1 && x0 >= 0 && x0 < row){
                     if (y0 >= 0 && y0 < col)
                     {
                         z00 = data_image[(row - x0 - 1) * col * 3 + y0 * 3 + c];
@@ -190,13 +173,6 @@ int main(int argc, char **argv)
     {
         data_image_file >> data_image[i];
     }
-    char filtered[data_image_n * data_image_m * 3];
-    int *dimage, *dqimage;
-    char *dfiltered;
-    int *drange;
-    cudaMalloc(&dimage, 4 * data_image_n * data_image_m * 3);
-    cudaMemcpyAsync(dimage, data_image, data_image_m * data_image_n * 3 * 4, cudaMemcpyHostToDevice);
-
     for (int i = 0; i < query_image_m * query_image_n * 3; i++)
     {
         query_image_file >> query_image[i];
@@ -206,6 +182,11 @@ int main(int argc, char **argv)
     query_image_file.close();
     // 0 = -45, 1 = 0, 2 = 45
 
+    char filtered[data_image_n * data_image_m * 3];
+    int *dimage, *dqimage;
+    char *dfiltered;
+    int *drange;
+
     int range[] = {0, 0, floor(-0.707 * query_image_n),
                    ceil((query_image_m + query_image_n) * 0.707), query_image_m, ceil(query_image_m * 0.707),
                    floor(-0.707 * query_image_m), 0, 0,
@@ -214,6 +195,7 @@ int main(int argc, char **argv)
     cudaMalloc(&drange, 12 * 4);
     cudaMalloc(&dfiltered, data_image_n * data_image_m * 3); // allocate an array for to mark the filtered values
     cudaMalloc(&dqimage, 4 * query_image_n * query_image_m * 3);
+    cudaMalloc(&dimage, data_image_n * data_image_m*3*4);
 
     cudaMemcpy(drange, range, 12 * 4, cudaMemcpyHostToDevice);
     cudaMemcpyAsync(dqimage, query_image, query_image_m * query_image_n * 3 * 4, cudaMemcpyHostToDevice);
@@ -224,50 +206,56 @@ int main(int argc, char **argv)
     int query_col = query_image_n;
 
     float query_grey = 0;
-    for (int i = 0; i < query_row * query_col * 3; i++)
-    { // calculating the grey average of the query
+    for (int i = 0; i < query_row * query_col * 3; i++){ // calculating the grey average of the query
         query_grey += query_image[i];
     }
     query_grey = query_grey / (query_image_m * query_image_n * 3);
     cout << "query grey " << query_grey << '\n';
-    int times = row * col / 2000 * 2000;
+
+    int times = (row * col)/ (1000 * 1000);
     int part_row_size = 0;
-    if (times != 0)
-    {
+
+    if (times != 0){
         int start_row;
         part_row_size = row / times;
+        int rooty = (col + 31) / 32;
+        int rootx = (part_row_size + 31) / 32;
+        dim3 blocks(rootx, rooty, 3);
+        dim3 threads(32, 32, 1); // many extra threads will be there
+        int size;
         for (start_row = 0; start_row < row; start_row += part_row_size)
-        {
-            int rooty = (col - query_col + 31) / 32;
-            int rootx = (part_row_size - query_row + 31) / 32;
-            dim3 blocks(rootx, rooty, 3);
-            dim3 threads(32, 32, 1); // many extra threads will be there
-
+        {   
+            if(start_row + part_row_size + query_row>row){
+                size = row - start_row;
+            }else{
+                size = part_row_size+ query_row;
+            }
+            cudaMemcpy(dimage, &data_image[start_row*col*3], (size)*col*3*4, cudaMemcpyHostToDevice);
             cudaDeviceSynchronize();
-            filter<<<blocks, threads>>>(dimage, drange, query_grey, row, col, query_row, query_col, TH2, dfiltered, start_row); // filtering
+
+            filter<<<blocks, threads>>>(dimage, drange, query_grey, part_row_size, col, query_row, query_col, TH2, dfiltered, start_row, row); // filtering
         }
     }
 
     part_row_size = row - part_row_size * times;
-    printf("part_row_size %d\n", part_row_size);
     int start_row = part_row_size * times;
-    printf("start_row %d\n", start_row);
-    if (part_row_size != 0)
+    if (part_row_size != 0 && row-start_row>=query_row)
     {
-        int rooty = (col - query_col + 31) / 32;
-        int rootx = (part_row_size - query_row + 31) / 32;
+        cudaMemcpy(dimage, &data_image[start_row*col*3], part_row_size*col*3*4, cudaMemcpyHostToDevice);
+        int rooty = (col + 31) / 32;
+        int rootx = (part_row_size + 31) / 32;
         dim3 blocks(rootx, rooty, 3);
         dim3 threads(32, 32, 1); // many extra threads will be there
         cudaDeviceSynchronize();
-        filter<<<blocks, threads>>>(dimage, drange, query_grey, row, col, query_row, query_col, TH2, dfiltered, start_row); // filtering
+        filter<<<blocks, threads>>>(dimage, drange, query_grey, part_row_size, col, query_row, query_col, TH2, dfiltered, start_row, row); // filtering
     }
-
+    
     cudaDeviceSynchronize();
     cudaMemcpy(filtered, dfiltered, row * col * 3, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
     cudaError_t err = cudaGetLastError();
     cout << cudaGetErrorString(err) << '\n';
 
+    cudaMemcpyAsync(dimage, data_image, row*col*3*4, cudaMemcpyHostToDevice);
     vector<int> coordinates;
     for (int x = 0; x < row; x++)
     {
@@ -301,6 +289,8 @@ int main(int argc, char **argv)
 
     int nblocks = (no_filtered + 1023) / 1024;
     // int nthreads = no_filtered/nblocks + no_filtered%nblocks;
+    cudaDeviceSynchronize();
+
     RMSDselect<<<nblocks, 1024>>>(dimage, dqimage, dcossin, dcoordinates, TH1, row, col, query_row, query_col, dtops, no_filtered);
 
     cudaDeviceSynchronize();
@@ -310,10 +300,8 @@ int main(int argc, char **argv)
     cudaDeviceSynchronize();
 
     vector<pair<float, float>> result;
-    for (int i = 0; i < no_filtered; i++)
-    {
-        if (tops[i] != -1)
-        {
+    for (int i = 0; i < no_filtered; i++){
+        if (tops[i] != -1){
             pair<float, float> temp;
             temp.first = i;
             temp.second = tops[i];
@@ -328,16 +316,13 @@ int main(int argc, char **argv)
         int i = result[j].first;
         printf("%0.3f,%d,%d,", result[j].second, coordinates[3 * i], coordinates[3 * i + 1]);
         int angle = coordinates[3 * i + 2];
-        if (angle == 0)
-        {
+        if (angle == 0){
             printf("-45\n");
         }
-        else if (angle == 1)
-        {
+        else if (angle == 1){
             printf("0\n");
         }
-        else if (angle == 2)
-        {
+        else if (angle == 2){
             printf("45\n");
         }
     }
